126be9cb61876f595d4c0bdfa8f45a33a6e13691
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-04-26 11:18:19 -0400

    responsive text styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-20">
-      <img src="/background-all.svg" alt="Background" className="w-full" />
-      <div className="flex flex-col items-center text-4xl w-1/2 justify-center">
+    <main className="flex min-h-screen flex-col items-center p-24 pt-20">
+      <img
+        src="/background-all.svg"
+        alt="Background"
+        className="w-full mb-12"
+      />
+      <div className="flex flex-col items-center text-center  text-xl md:text-2xl lg:text-4xl w-full md:w-2/3 lg:w-1/2 justify-center">
         <span className="text-[#2BAFF6]">
           Ever wondered what it’s like to live in the futuristic utopia of BOLT
           12?
