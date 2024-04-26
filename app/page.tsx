b76d0af3c2339facfee8839ed03c6fd25c9fcdde
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-04-26 11:11:54 -0400

    intro page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className=" flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-20">
+      <img src="/background-all.svg" alt="Background" className="w-full" />
+      <div className="flex flex-col items-center text-4xl w-1/2 justify-center">
+        <span className="text-[#2BAFF6]">
+          Ever wondered what it’s like to live in the futuristic utopia of BOLT
+          12?
+        </span>
+        <span className="text-[#E64500]">Let’s take a look!</span>
+      </div>
     </main>
   );
 }
