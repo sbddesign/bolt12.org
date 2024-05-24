e0b895efe880a16f7e5d9aa5077731c1ca62a5d1
Stephen DeLorme <stephen@d.elor.me>
2024-05-24 15:17:31 -0400

    user profiles updated text

--- features/UserProfile.tsx
+++ features/UserProfile.tsx
@@ -1,19 +1,28 @@
 import Image from "next/image"
+import { ArrowRightIcon } from "@bitcoin-design/bitcoin-icons-react/filled";
 
-interface UserProfileProps {
+export type UserProfileProps = {
     userName: string;
     featureName: string;
     imageSrc: string;
-    config: "even" | "odd";
+    config?: "even" | "odd";
     description: string;
     link: string;
     linkText?: string;
 }
 
-export default function UserProfile(){
+export default function UserProfile(props:UserProfileProps){
     return(
         <>
-            <Image src={"/users/alice@2x.png"} alt="Alice" width={200} height={200} className="w-1/3" />
+            <div className="w-full flex flex-col gap-4">
+                <Image src={props.imageSrc} alt="Alice" width={200} height={200} className="w-full sm:w-2/3" />
+                <h3 className="text-[#6F4EA6] font-headings text-4xl"><span className="font-display block">{props.userName} loves</span> {props.featureName}</h3>
+                <p>{props.description}</p>
+                <a href={props.link} className="flex flex-row gap-2 text-[#37869F] font-semibold text-2xl items-center">
+                    <span className="inline-block">{props.linkText || "See User Profile"}</span>
+                    <ArrowRightIcon className="w-6 h-6 " />
+                </a>
+            </div>
         </>
     )
 }
