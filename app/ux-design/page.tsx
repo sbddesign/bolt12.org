48d8a470dc59fe67de48ce4855a676e7a3a4ca2f
Stephen DeLorme <stephen@d.elor.me>
2024-05-24 12:57:55 -0400

    lint appease

--- app/ux-design/page.tsx
+++ app/ux-design/page.tsx
@@ -14,7 +14,7 @@ export default function UxDesign(){
                     SHA-256, fee market hodl block reward outputs roller coaster volatility wallet mempool! Price action deflationary monetary policy hyperbitcoinization sats, peer-to-peer SHA-256 key pair! Money printer go brrrrr pizza roller coaster soft fork address Satoshi Nakamoto, price action, halvening! Soft fork, fee market space citadel double-spend problem.
                 </p>
                 <p>
-                    Pizza sats blockchain stacking sats, volatility key pair to the moon segwit. Wallet Merkle Tree genesis block, whitepaper, money printer go brrrrr outputs block reward segwit whitepaper! Outputs transaction, mining inputs mempool space citadel, price action transaction? Price action consensus mempool electronic cash I'm in it for the tech, mining consensus digital signature? Mining, key pair, decentralized?
+                    Pizza sats blockchain stacking sats, volatility key pair to the moon segwit. Wallet Merkle Tree genesis block, whitepaper, money printer go brrrrr outputs block reward segwit whitepaper! Outputs transaction, mining inputs mempool space citadel, price action transaction? Price action consensus mempool electronic cash I&rsquo;m in it for the tech, mining consensus digital signature? Mining, key pair, decentralized?
                 </p>
                 <p>
                     Private key hodl address miner soft fork pizza blockchain when lambo! Digital signature Bitcoin Improvement Proposal transaction public key money printer go brrrrr satoshis peer-to-peer. Roller coaster digital signature address SHA-256, digital signature money printer go brrrrr decentralized, segwit. Mining.
