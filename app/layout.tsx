8329b89fc7309124cb7ae4379e49742dc377cc08
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-05-27 18:30:09 -0400

    add og metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,13 +12,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" prefix="og: https://ogp.me/ns#">
       <head>
         <title>Bolt12</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Pacifico&family=Pixelify+Sans:wght@400..700&family=Protest+Revolution&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://use.typekit.net/pqu3bot.css"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Pacifico&family=Pixelify+Sans:wght@400..700&family=Protest+Revolution&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/pqu3bot.css"
+        ></link>
+        <meta property="og:title" content="BOLT 12" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://bolt12.org" />
+        <meta property="og:image" content="/public/bolt12-logo.svg" />
+        <meta
+          property="og:description"
+          content="BOLT 12 implements offers, which improves Lightning QR codes by making them smaller, reusable, and able to send as well as receive money."
+        />
       </head>
       <body className={``}>{children}</body>
     </html>
