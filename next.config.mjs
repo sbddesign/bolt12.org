033c17534401000ea984a74a05d02fb99a4bc640
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-05-28 21:28:26 -0400

    add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/bolt12.html",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
